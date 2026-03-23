fff458a2eb82ff9565b4db22cd51e09e6579d34b
Jake Farrell <jake.farrell88@mail.dcu.ie>
2026-03-23 21:10:07 +0000

    fix test

--- app/settings.py
+++ app/settings.py
@@ -27,7 +27,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "True").strip().lower() == "true"
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",") if os.getenv("ALLOWED_HOSTS") else []
 
 # Application definition
 
